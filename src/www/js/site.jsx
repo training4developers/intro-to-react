01746a8827851385af9f1591012fabed4cabc7f9
Eric Greene <Eric Greene>
2016-02-28 09:53:59 -0700

    cleaned up files

--- src/www/js/site.jsx
+++ src/www/js/site.jsx
@@ -1,14 +1,2 @@
 "use strict";
 
-const
-	React = require("react"),
-	ReactDOM = require("react-dom"),
-	Header = require("./components/header"),
-	WidgetsTool = require("./components/widgets-tool"),
-	Footer = require("./components/footer");
-
-ReactDOM.render(<div>
-		<Header />
-		<WidgetsTool />
-		<Footer />
-	</div>, document.querySelector("body > div:first-child"));
